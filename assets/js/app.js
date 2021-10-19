4e8a0f5b4c4f57bd6bf4b20023168f1d0a65fdae
Nurdaulet123456 <91106511+Nurdaulet123456@users.noreply.github.com>
2021-10-19 18:03:53 +0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,11 @@
 $(function() {
 
     let intro = $("#intro");
-    let content = $("#content");
     let header = $("#header");
     let subnav = $("#subnav");
     let subnav2 = $(".subnav");
     let search = $("#search");
     let introHight = intro.innerHeight();
-    let contentHight = content.innerHeight();
     let headerHight = header.innerHeight();
     let scrollTop = $(window).scrollTop();
 
@@ -25,8 +23,8 @@ $(function() {
         headerHight = header.innerHeight();
 
         let topScroll = $(this).scrollTop();
-
-        if (topScroll >= (introHight - headerHight)) {
+        let scrollSpy = introHight - headerHight;
+        if (topScroll >= scrollSpy) {
             header.addClass("header--darken");
             subnav.addClass("subnav--darken");
             subnav2.addClass("subnav--darken");
@@ -104,36 +102,26 @@ $(function() {
 
     // BurgerToggle
 
-    let navToggle = $('#navToggle');
-    let nav = $('#nav');
 
-    navToggle.on('click', function(event) {
+
+
+    $('#navToggle').on('click', function(event) {
         event.preventDefault();
 
 
 
         $(this).toggleClass('active');
-        nav.toggleClass('show');
+        $('#nav').toggleClass('show');
     });
 
     $(window).on('resize', function() {
 
         $("body").removeClass('show-nav');
-        navToggle.removeClass('active');
-        nav.removeClass('show');
+        $('#navToggle').removeClass('active');
+        $('#nav').removeClass('show');
 
     });
 
-
-    /* Typed.js */
-
-    var typed = new Typed(".type", {
-        strings: ["Everyday", "Fullday"],
-        typeSpeed: 60,
-        backSpeed: 60,
-        loop: true
-
-    });
 
     /* Move-up */
 
@@ -143,7 +131,7 @@ $(function() {
         $('html, body').animate({
             scrollTop: 0
 
-        }, 500);
+        }, 200);
     });
 
     // Header__logo Scroll
@@ -154,54 +142,8 @@ $(function() {
         $('html, body').animate({
             scrollTop: 0
 
-        }, 500);
+        }, 200);
     });
-
-
-
-    /* AOS.js https://github.com/michalsnik/aos */
-
-
-    AOS.init({
-        // Global settings:
-        disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-        initClassName: 'aos-init', // class applied after initialization
-        animatedClassName: 'aos-animate', // class applied on animation
-        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-
-        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 150, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 1000, // values from 0 to 3000, with step 50ms
-        easing: 'ease', // default easing for AOS animations
-        once: false, // whether animation should happen only once - while scrolling down
-        mirror: false, // whether elements should animate out while scrolling past them
-        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
